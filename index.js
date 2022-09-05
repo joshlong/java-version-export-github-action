6704b9da64ab9a078e406e85dd4c7202a46f32ea
Josh Long <josh@joshlong.com>
2022-09-04 21:02:47 -0700

    polish

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
 /**
- * This Github Action will resolve all environment variables suffixed with one of the
- * two `BP_MODE` values (`DEVELOPMENT`, `PRODUCTION`) and will canonicalize them
- * based on what value the environment variable `BP_MODE` has.
+ * Uses Bruno Borges' brilliant hack to extract a property from a Maven `pom.xml` and export it to an environment variable
+ * or property that other things, like the `setup-java` Github Action, can use.
  *
  * @author Josh Long
  */
-const core = require('@actions/core');
-const github = require('@actions/github');
+// https://github.com/actions/toolkit
+const core = require("@actions/core");
+const github = require("@actions/github");
+const github = require("@actions/github");
 
 // todo analyze the incoming event payload and then set BP_MODE to be something useful
 try {
-
-  function resolveGithubAction() {
-    // we assume development by default; production only in
-    // a very specific, purposeful scenario
-    const {context} = require("@actions/github");
-    if (context && context.payload && context.payload.action)
-      return context.payload.action;
-    return null
-  }
-
-  const action = resolveGithubAction();
-  // console.log(action)
-  // console.log(github.context)
-  // console.log(github.context.payload)
-  const bpMode = action === 'deploy-production-event' ? 'production' : 'development'
-
-  core.exportVariable('BP_MODE', bpMode.toUpperCase())
-  core.exportVariable('BP_MODE_LOWERCASE', bpMode.toLowerCase())
-
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  console.log('the BP_MODE is ' + bpMode)
-  for (let k in process.env) {
-    const isForThisEnvironment = k.toLowerCase().endsWith('_' + bpMode)
-    if (isForThisEnvironment) {
-      const sansSuffix = k.substring(0, k.length - (1 + bpMode.length))
-      core.exportVariable(sansSuffix, process.env[k])
-      console.log(`exporting ${sansSuffix} to have the value of ${k}`)
-    }
-  }
+  const toEval =
+    "mvn help:evaluate -Dexpression=java.version -q -DforceStdout ";
+  console.log(toEval);
+  const result = await exec.exec("mvn", [
+    "help:evaluate",
+    "-q",
+    "-DforceStdout",
+    "-Dexpression=java.version",
+  ]);
+  console.log("the result is " + result);
+  core.exportVariable("MAVEN_PROPERTY_EVALUATION", bpMode.toUpperCase());
 } catch (error) {
   core.setFailed(error.message);
 }
-
